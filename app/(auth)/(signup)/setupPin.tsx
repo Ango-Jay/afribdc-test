c5828eaa28074142cd8034d7ad379864f5008892
Ango Jeffrey <angojeff433@gmail.com>
2025-03-06 21:23:04 +0100

    added login and setup pin ui

--- app/(auth)/(signup)/setupPin.tsx
+++ app/(auth)/(signup)/setupPin.tsx
@@ -1,3 +1,53 @@
+import CustomButton from '@/components/shared/Button';
+import BackButton from '@/components/shared/Button/BackButton';
+import {PinFieldGroup, PinKeypad} from '@/components/shared/Form/Pin';
+import {LayoutWithScroll} from '@/components/shared/Layout/LayoutWithScroll';
+import CustomText from '@/components/shared/Text';
+import globalUtilStyles from '@/styles';
+import {useState} from 'react';
+import {View} from 'react-native';
+
 export default function SetupPin() {
-  return <></>;
+  const [pin, setPin] = useState<string[]>([]);
+  return (
+    <LayoutWithScroll>
+      <View style={[globalUtilStyles.flex1]}>
+        <View
+          style={[
+            globalUtilStyles.flexRow,
+            globalUtilStyles.itemsCenter,
+            globalUtilStyles.mb10,
+          ]}>
+          <BackButton style={[globalUtilStyles.absolute]} />
+          <CustomText size={20} style={[globalUtilStyles.mxauto]}>
+            Set Your Pin
+          </CustomText>
+        </View>
+        <View
+          style={[
+            globalUtilStyles.wfull,
+            globalUtilStyles.itemsCenter,
+            globalUtilStyles.mb10,
+          ]}>
+          <CustomText
+            style={[
+              globalUtilStyles.textCenter,
+              {
+                width: '95%',
+              },
+            ]}>
+            Set up your 4 digit pin to make your account more secure. You’ll be
+            asked to enter this pin when making transcations.
+          </CustomText>
+        </View>
+        <View style={[globalUtilStyles.gap6]}>
+          <PinFieldGroup type="pin" pin={pin} maxLength={4} />
+          <View style={[globalUtilStyles.wfull]}>
+            <CustomButton text="Proceed" />
+          </View>
+          <PinKeypad pin={pin} setPin={setPin} maxLength={4} />
+        </View>
+      </View>
+    </LayoutWithScroll>
+  );
 }
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,3 +1,103 @@
+import CustomButton from '@/components/shared/Button';
+import BackButton from '@/components/shared/Button/BackButton';
+import CustomTextInput from '@/components/shared/Form/CustomInput';
+import PasswordInput from '@/components/shared/Form/PasswordInput';
+import {LayoutWithScroll} from '@/components/shared/Layout/LayoutWithScroll';
+import CustomText from '@/components/shared/Text';
+import globalUtilStyles from '@/styles';
+import {textColorStyle} from '@/styles/color';
+import {Controller, SubmitHandler, useForm} from 'react-hook-form';
+import {View} from 'react-native';
+
 export default function Login() {
-  return <></>;
+  const {
+    control,
+    formState: {errors},
+    handleSubmit,
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const onSubmit: SubmitHandler<FormValues> = () => {
+    // do something
+  };
+  return (
+    <LayoutWithScroll>
+      <View style={[globalUtilStyles.flex1]}>
+        <View style={[globalUtilStyles.mb10]}>
+          <BackButton />
+        </View>
+        <View
+          style={[
+            globalUtilStyles.wfull,
+            globalUtilStyles.mt10,
+            globalUtilStyles.itemsCenter,
+            globalUtilStyles.gap3,
+          ]}>
+          <CustomText size={20} weight={600}>
+            Login to your account
+          </CustomText>
+          <View style={[globalUtilStyles.wfull]}>
+            <Controller
+              control={control}
+              name="email"
+              render={({field}) => (
+                <CustomTextInput
+                  {...field}
+                  labelTitle="Email"
+                  placeholder="Email"
+                  errorMessage={errors.email?.message}
+                  isEmail
+                />
+              )}
+            />
+          </View>
+          <View style={[globalUtilStyles.wfull]}>
+            <Controller
+              control={control}
+              name="password"
+              render={({field}) => (
+                <PasswordInput
+                  {...field}
+                  labelTitle="Create password"
+                  placeholder="Password"
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+          </View>
+          <View
+            style={[
+              globalUtilStyles.wfull,
+              globalUtilStyles.gap4,
+              globalUtilStyles.mt8,
+            ]}>
+            <View style={[globalUtilStyles.wfull]}>
+              <CustomText
+                weight={500}
+                style={[textColorStyle.highlight, globalUtilStyles.mlauto]}>
+                Forgot Password?
+              </CustomText>
+            </View>
+            <CustomButton onPress={handleSubmit(onSubmit)} text="Login" />
+            <View style={[globalUtilStyles.itemsCenter]}>
+              <CustomText weight={500}>
+                Don't have an account?{' '}
+                <CustomText weight={500} style={[textColorStyle.highlight]}>
+                  Signup
+                </CustomText>
+              </CustomText>
+            </View>
+          </View>
+        </View>
+      </View>
+    </LayoutWithScroll>
+  );
 }
+
+type FormValues = {
+  email: string;
+  password: string;
+};
